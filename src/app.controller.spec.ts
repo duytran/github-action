8973709cb33c3cea49f3767b02b19d88de0986bd
Duy Tran <duytran@asnet.com.vn>
2023-07-07 17:27:53 +0700

    feat: add failling unit test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,7 +15,7 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
+    it.failing('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
   });
