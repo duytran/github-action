7ce0dec23f8d9e2630fcada63171382111ca0cfe
Duy Tran <duytran@asnet.com.vn>
2023-07-07 17:30:14 +0700

    feat: fix unit test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,7 +15,7 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it.failing('should return "Hello World!"', () => {
+    it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
   });
